a4cd151a5a9c9f863ac9d6eb9b99e7e12ddda5bb
Thomas Basham <Bashamtg@gmail.com>
2024-05-04 09:37:17 -0700

    added async debug code

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
-
 console.log("Hello world!");
 // global scope
 let likes = "";
 
 function setLikes() {
-  debugger
   // block scope
   for (let i = 0; i < 10; i++) {
     likes = likes + "❤️";
   }
 }
 
+if (false) {
+  debugger; // will not run
+}
+
 setLikes();
 
 console.log(likes);
+
+fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((error) => {
+    console.log(error);
+    // notify user there was an error
+    // extra code to keep the app running with the error
+  });
